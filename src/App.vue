0b14d81e3867eae1365c26b950da4a522113ec1f
eduregis <eduregis1@gmail.com>
2019-06-09 21:59:43 -0300

    Página de áreas de interesses

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app dark>
+    <v-toolbar light color="#fdd835">
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Eduardo Regis </span>        
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-layout justify-center class="mb-2">
+        <v-btn flat>          
+          Sobre mim          
+        </v-btn>        
+        <v-btn flat>
+          Projetos          
+        </v-btn>
+        <v-btn flat>
+          Interesses          
+        </v-btn>        
+      </v-layout>  
+      <v-spacer></v-spacer>
+      <v-btn flat>
+        <span class="mr-2">Entre em Contato!</span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <Interests/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Interests from './components/Interests'
+
+export default {
+  name: 'App',
+  components: {
+    Interests
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  background: black;
+}
+</style>
+
