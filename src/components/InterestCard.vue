47ddcc73a60019e9a5c0d24cb93657907f73a4a3
eduardo <eduregis1@gmail.com>
2019-08-13 13:51:49 -0300

    usando tooltips para descrição de interesse

--- src/components/InterestCard.vue
+++ src/components/InterestCard.vue
@@ -1,36 +1,37 @@
 <template>
-    <v-card 
-        class="interest-card"
-        ripple
-        hover
-        @click="switchInfo = !switchInfo"
-    > 
-        <div v-if="switchInfo">
-            <v-card-actions class="justify-center">            
-                <div class="interest-circle" :class="card.color">
-                    <img
-                        :src="card.img"
-                        height="32"
-                        width="32"
-                        :alt="card.title"
-                    />
-                </div>                       
-            </v-card-actions>  
-            <v-divider></v-divider>
-            <v-card-actions class="justify-center">
-                <div class="interest-title">
-                    <h2 class="display-1 light--text font-weight-light">{{ card.title }}</h2>  
-                </div> 
-            </v-card-actions>
-        </div> 
-        <div v-else>
-            <v-card-actions>
-                <div class="interest-desc">
-                    <p>{{ card.description }}</p>
-                </div>
-            </v-card-actions>
-        </div>                  
-    </v-card>
+    <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+            <v-card 
+                class="interest-card"
+                ripple
+                hover
+                v-on="on"
+            > 
+                <div>
+                    <v-card-actions class="justify-center">            
+                        <div class="interest-circle" :class="card.color">
+                            <img
+                                :src="card.img"
+                                height="32"
+                                width="32"
+                                :alt="card.title"
+                            />
+                        </div>                       
+                    </v-card-actions>  
+                    <v-divider></v-divider>
+                    <v-card-actions class="justify-center">
+                        <div class="interest-title">
+                            <h2 class="display-1 light--text font-weight-light">{{ card.title }}</h2>  
+                        </div> 
+                    </v-card-actions>
+                </div>         
+            </v-card>
+        </template>
+        <div class="interest-desc">
+            <span>{{ card.description }}</span>
+        </div>        
+    </v-tooltip>
+    
 </template>
 
 <script>
@@ -38,11 +39,6 @@ export default {
     props: [
         'card'
     ],
-    data(){
-        return {
-            switchInfo: true,
-        }
-    }
 }
 </script>
 
@@ -58,7 +54,7 @@ export default {
     border-radius: 50%;  
 }
 .interest-desc {
-    margin: 10px auto 10px auto;
+    width: 300px;
 }
 </style>
 
